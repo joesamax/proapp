d9e5eddce9031cbecec73dc261e77728673a0ed9
joesamax <johan.galipo@gmail.com>
2020-06-12 10:49:32 +0200

    removal yeah i said it twice

--- wp-content/plugins/mobiloud-mobile-app-plugin/views/welcome_footer.php
+++
@@ -1,6 +0,0 @@
-<div class="clear"></div>
-
-	<div class="ml2-contact-us">
-		Already a customer and want to start using the plugin? <a href="#" data-href="<?php echo( esc_attr( $active_url ) ); ?>" class="welcome_question_start">Click here to manage your app</a>.
-	</div>
-</div> <!-- about-wrap -->
